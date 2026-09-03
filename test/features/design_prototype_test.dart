3ee69d35a8bd018b34ac5db12fd87861bacd0fa8
qbjsdsb <3342513714@qq.com>
2026-09-03 16:49:12 +0800

    import keyboard key definitions for shortcut regression

--- test/features/design_prototype_test.dart
+++ test/features/design_prototype_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/semantics.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:xueqing/app/theme/app_theme.dart';
