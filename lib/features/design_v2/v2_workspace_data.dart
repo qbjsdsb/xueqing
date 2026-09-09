aaaf830f8007e4bedddbda0555f1bf819efcb670
qbjsdsb <3342513714@qq.com>
2026-09-09 21:28:43 +0800

    ui(v2): provide workspace data scope

--- lib/features/design_v2/v2_workspace_data.dart
+++ lib/features/design_v2/v2_workspace_data.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/widgets.dart';
+
 import 'v2_fixture.dart';
 
 /// Presentation-only data contract for the V2 workspace.
@@ -33,6 +35,27 @@ class V2WorkspaceData {
         .where((entry) => caseIds.contains(entry.caseId))
         .toList(growable: false);
   }
+}
+
+class V2WorkspaceDataScope extends InheritedWidget {
+  const V2WorkspaceDataScope({
+    required this.data,
+    required super.child,
+    super.key,
+  });
+
+  final V2WorkspaceData data;
+
+  static V2WorkspaceData of(BuildContext context) {
+    final scope = context
+        .dependOnInheritedWidgetOfExactType<V2WorkspaceDataScope>();
+    assert(scope != null, 'V2WorkspaceDataScope is missing above this widget.');
+    return scope!.data;
+  }
+
+  @override
+  bool updateShouldNotify(V2WorkspaceDataScope oldWidget) =>
+      !identical(data, oldWidget.data);
 }
 
 const v2FixtureWorkspaceData = V2WorkspaceData(
