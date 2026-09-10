3b59c4e875e940bef7a82c654f8f9d21d905c470
qbjsdsb <3342513714@qq.com>
2026-09-11 01:19:04 +0800

    Center the initial Windows app window

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -5,6 +5,43 @@
 #include "flutter_window.h"
 #include "utils.h"
 
+namespace {
+
+void CenterWindowInWorkArea(HWND window) {
+  if (window == nullptr) {
+    return;
+  }
+
+  RECT window_rect{};
+  if (!::GetWindowRect(window, &window_rect)) {
+    return;
+  }
+
+  HMONITOR monitor = ::MonitorFromWindow(window, MONITOR_DEFAULTTONEAREST);
+  MONITORINFO monitor_info{};
+  monitor_info.cbSize = sizeof(monitor_info);
+  if (!::GetMonitorInfo(monitor, &monitor_info)) {
+    return;
+  }
+
+  const int window_width = window_rect.right - window_rect.left;
+  const int window_height = window_rect.bottom - window_rect.top;
+  const int work_width = monitor_info.rcWork.right - monitor_info.rcWork.left;
+  const int work_height = monitor_info.rcWork.bottom - monitor_info.rcWork.top;
+
+  const int x = work_width > window_width
+                    ? monitor_info.rcWork.left + (work_width - window_width) / 2
+                    : monitor_info.rcWork.left;
+  const int y = work_height > window_height
+                    ? monitor_info.rcWork.top + (work_height - window_height) / 2
+                    : monitor_info.rcWork.top;
+
+  ::SetWindowPos(window, nullptr, x, y, 0, 0,
+                 SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
+}
+
+}  // namespace
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
@@ -30,6 +67,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   if (!window.Create(L"\u5B66\u60C5", origin, size)) {
     return EXIT_FAILURE;
   }
+  CenterWindowInWorkArea(window.GetHandle());
   window.SetQuitOnClose(true);
 
   ::MSG msg;
@@ -41,4 +79,3 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   ::CoUninitialize();
   return EXIT_SUCCESS;
 }
-
