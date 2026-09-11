c097bdaf847589e61ed43737bd4352ba34b173c8
qbjsdsb <3342513714@qq.com>
2026-09-11 16:16:19 +0800

    release: align v0.3.5 runtime version defaults

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -59,7 +59,7 @@ class AppConfig {
           : normalizedEnvironment,
       appVersion: const String.fromEnvironment(
         'XUEQING_APP_VERSION',
-        defaultValue: '0.3.4+12',
+        defaultValue: '0.3.5+13',
       ),
       cloudConfig: CloudConfig.fromDartDefines(),
       showDeveloperTools: showDeveloperTools,
@@ -77,7 +77,7 @@ class AppConfig {
 
   factory AppConfig.fromValues({
     String environmentValue = 'development',
-    String appVersion = '0.3.4+12',
+    String appVersion = '0.3.5+13',
     CloudConfig cloudConfig = const CloudConfig(),
     bool? showDeveloperTools,
   }) {
