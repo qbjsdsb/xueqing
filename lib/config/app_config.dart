f6215aa23bf6f144393fa59d5ad5624c75f21eb1
qbjsdsb <3342513714@qq.com>
2026-09-11 22:39:56 +0800

    release: freeze v0.3.6+14 runtime version

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -59,7 +59,7 @@ class AppConfig {
           : normalizedEnvironment,
       appVersion: const String.fromEnvironment(
         'XUEQING_APP_VERSION',
-        defaultValue: '0.3.5+13',
+        defaultValue: '0.3.6+14',
       ),
       cloudConfig: CloudConfig.fromDartDefines(),
       showDeveloperTools: showDeveloperTools,
@@ -77,7 +77,7 @@ class AppConfig {
 
   factory AppConfig.fromValues({
     String environmentValue = 'development',
-    String appVersion = '0.3.5+13',
+    String appVersion = '0.3.6+14',
     CloudConfig cloudConfig = const CloudConfig(),
     bool? showDeveloperTools,
   }) {
