3a5b9a977a6f723384d76f12368ec9bc6e395f18
qbjsdsb <3342513714@qq.com>
2026-09-14 21:59:46 +0800

    release: freeze v0.3.12 version
    Freeze Xueqing stable version at 0.3.12+20 after full Flutter, Android and Windows release gates passed.

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -59,7 +59,7 @@ class AppConfig {
           : normalizedEnvironment,
       appVersion: const String.fromEnvironment(
         'XUEQING_APP_VERSION',
-        defaultValue: '0.3.11+19',
+        defaultValue: '0.3.12+20',
       ),
       cloudConfig: CloudConfig.fromDartDefines(),
       showDeveloperTools: showDeveloperTools,
@@ -77,7 +77,7 @@ class AppConfig {
 
   factory AppConfig.fromValues({
     String environmentValue = 'development',
-    String appVersion = '0.3.11+19',
+    String appVersion = '0.3.12+20',
     CloudConfig cloudConfig = const CloudConfig(),
     bool? showDeveloperTools,
   }) {
